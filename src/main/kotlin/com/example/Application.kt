9cfd095a48d62904b7a733f93cf42a7e7b2e3a83
thekaailashsharma <kailashps.1011@gmail.com>
2024-05-20 03:22:05 +0530

    Initial Commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    embeddedServer(
+        Netty,
+        port = 8082,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
